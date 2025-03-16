7348e90936c722d5d433e5d8dc6d8dafdd45bd1f
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-03-16 21:56:30 +0900

    Delete Python directory

--- Python/15596.py
+++
@@ -1,6 +0,0 @@
-def solve(a):
-    return sum(a) # sum()이 훨 씬 빠름 5~10배
-
-# def solve(a):
-#     for i in range(a):
-#         sum += i
--- Python/2577.py
+++
@@ -1,8 +0,0 @@
-
-A = int(input())
-B = int(input())
-C = int(input())
-X = list(str(A * B * C)) # 곱셈값 숫자 하나하나 문자열로 list
-
-for i in range(10):
-    print(X.count(str(i))) # 문자열로 저장된 숫자를 카운트 해야해서 str(i)로 해야함
