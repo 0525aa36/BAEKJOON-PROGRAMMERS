d9917d070dcd974a52481cfa06133b4c8faae407
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-03-27 11:08:35 +0900

    [Silver V] Title: 숫자 카드, Time: 400 ms, Memory: 121096 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,10 +1,7 @@
 import sys
 
-
 N = int(sys.stdin.readline().strip()) # 상근이 카드개수
 N_card = set(map(int, sys.stdin.readline().split())) #상근이카드
-
-
 M = int(sys.stdin.readline().strip()) #확인카드개수
 M_card = list(map(int, sys.stdin.readline().split())) # g확인카드
 
