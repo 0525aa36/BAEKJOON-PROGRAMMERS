5a12c32343ccc26e2e3bd09952264e214058df87
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-03-27 10:12:51 +0900

    [Silver V] Title: 숫자 카드, Time: 400 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -2,16 +2,16 @@ import sys
 
 
 N = int(sys.stdin.readline().strip()) # 상근이 카드개수
-N_cards = set(map(int, sys.stdin.readline().split())) #상근이카드
+N_card = set(map(int, sys.stdin.readline().split())) #상근이카드
 
 
 M = int(sys.stdin.readline().strip()) #확인카드개수
-queries = list(map(int, sys.stdin.readline().split())) # g확인카드
+M_card = list(map(int, sys.stdin.readline().split())) # g확인카드
 
 
 result = [] # 상근이 카드랑 확인카드 있으면 1 없으면 0
-for q in queries:
-    if q in N_cards:
+for i in M_card:
+    if i in N_card:
         result.append("1")
     else:
         result.append("0")
