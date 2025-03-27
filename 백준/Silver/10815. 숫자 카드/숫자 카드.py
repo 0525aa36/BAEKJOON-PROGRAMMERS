bc52ca7ff6862d338092a5d765c67beb745ccc37
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-03-27 11:09:28 +0900

    [Silver V] Title: 숫자 카드, Time: 392 ms, Memory: 122120 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,18 +1,17 @@
 import sys
 
-N = int(sys.stdin.readline().strip()) # 상근이 카드개수
-N_card = set(map(int, sys.stdin.readline().split())) #상근이카드
+S = int(sys.stdin.readline().strip()) # 상근이 카드개수
+S_card = set(map(int, sys.stdin.readline().split())) #상근이카드
 M = int(sys.stdin.readline().strip()) #확인카드개수
 M_card = list(map(int, sys.stdin.readline().split())) # g확인카드
 
 
 result = [] # 상근이 카드랑 확인카드 있으면 1 없으면 0
 for i in M_card:
-    if i in N_card:
+    if i in S_card:
         result.append("1")
     else:
         result.append("0")
 
-
-print(' '.join(result))
+print(' '.join(result)) #공백으로 구분해 출력한다라고 돼있음
 
