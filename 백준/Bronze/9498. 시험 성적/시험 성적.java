9baf062cbac021afff65c96e417150e7eea84d34
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-09-15 19:22:14 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int A = in.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        in.close();
-
-        if(A>=90) System.out.println("A");
-        else if(A>=80) System.out.println("B");
-        else if(A>=70) System.out.println("C");
-        else if(A>=60) System.out.println("D");
-        else System.out.println("F");
+        if (n >= 90) {
+            System.out.println("A");
+        } else if (n >= 80) {
+            System.out.println("B");
+        } else if (n >= 70) {
+            System.out.println("C");
+        } else if (n >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        } 
     }
 }
