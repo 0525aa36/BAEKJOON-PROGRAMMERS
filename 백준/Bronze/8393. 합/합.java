4d5fe6a39b05317bd2c80492003ee04b87622914
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-09-16 14:29:33 +0900

    [Bronze V] Title: 합, Time: 68 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < N + 1; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
