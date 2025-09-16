03d1ea392478f1313bf76d2966e9c8a3c6a228ff
0525aa36 <66399518+0525aa36@users.noreply.github.com>
2025-09-16 16:44:25 +0900

    [Bronze V] Title: 개수 세기, Time: 64 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,34 +1,32 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int count = 0;
         int[] arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());;
-
-        for(int i = 0; i < N; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int count = 0;
         int b = Integer.parseInt(br.readLine());
-
-        for(int j = 0; j < arr.length; j++){
-            if(b == arr[j]) {
+        for (int j = 0; j < N; j++) {
+            if(b == arr[j]){
                 count++;
             }
         }
-        System.out.println(count);
-
-        br.close();
+        bw.write(count + "\n");
         bw.flush();
-        bw.close();
     }
 }
+
